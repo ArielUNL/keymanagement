144fe8c622ac4647ac16bd584728d4fb0e20f337
Ariel U.N <111918016+ArielUNL@users.noreply.github.com>
2022-09-02 17:34:22 -0300

    Add files via upload

--- DBTesting/src/Main/Registros.java
+++ DBTesting/src/Main/Registros.java
@@ -7,6 +7,8 @@ import java.sql.PreparedStatement;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class Registros extends javax.swing.JFrame {
@@ -32,6 +34,10 @@ public class Registros extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jTextField5 = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
+        jButton4 = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField6 = new javax.swing.JTextField();
+        jTextField7 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -50,9 +56,19 @@ public class Registros extends javax.swing.JFrame {
         jPanel1.add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 30, 70, -1));
 
         jButton1.setText("RMV");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 60, 70, -1));
 
         jButton2.setText("UPT");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
         jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 60, 70, -1));
 
         jButton3.setText("ADD");
@@ -70,6 +86,23 @@ public class Registros extends javax.swing.JFrame {
         jLabel6.setText("ID");
         jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 30, -1, -1));
 
+        jButton4.setText("RLD");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 60, -1, -1));
+
+        jTextField1.setEditable(false);
+        jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 90, 70, -1));
+
+        jTextField6.setEditable(false);
+        jPanel1.add(jTextField6, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 90, 70, -1));
+
+        jTextField7.setEditable(false);
+        jPanel1.add(jTextField7, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 90, 70, -1));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -78,7 +111,7 @@ public class Registros extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 124, Short.MAX_VALUE)
         );
 
         pack();
@@ -87,7 +120,7 @@ public class Registros extends javax.swing.JFrame {
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         try{
             //Recolha os Valores p/ Botar na DB
-            String Valor_1 = jTextField2.getText();
+            String Valor_1 = jTextField5.getText();
             String Valor_2 = jTextField3.getText();
             String Valor_3 = jTextField4.getText();
             
@@ -96,7 +129,7 @@ public class Registros extends javax.swing.JFrame {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost/dbtesting","root","");
             
             //Configure o Tipo de Dados e a Atuação
-            PreparedStatement ps = con.prepareStatement("insert into javadbtesting(Valor_1,Valor_2,Valor_3)values(?,?,?");
+            PreparedStatement ps = con.prepareStatement("insert into javadbtesting(Valor_1,Valor_2,Valor_3)values(?,?,?)");
             
             //Atribua os Dados | Posição, Dado
             ps.setString(1, Valor_1);
@@ -108,9 +141,94 @@ public class Registros extends javax.swing.JFrame {
             
             JOptionPane.showMessageDialog(null, "Registro Concluído");
         }catch(Exception ex){
-            
+            Logger.getLogger(Registros.class.getName()).log(Level.SEVERE, null, ex);
         }
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        try{
+            //Recolha os Valores p/ Botar na DB
+            String Valor_1 = jTextField5.getText();
+            String Valor_2 = jTextField3.getText();
+            String Valor_3 = jTextField4.getText();
+            String id = jTextField2.getText(); // Pegar ID
+            
+            //Configure a Conexão do Banco de Dados
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/dbtesting","root","");
+            
+            //Configure o Tipo de Dados e a Atuação
+            PreparedStatement ps = con.prepareStatement("update javadbtesting set Valor_1=?,Valor_2=?,Valor_3=? where id=?");
+            
+            //Atribua os Dados | Posição, Dado
+            ps.setString(1, Valor_1);
+            ps.setString(2, Valor_2);
+            ps.setString(3, Valor_3);
+            ps.setString(4, id); // Campo de ID
+            
+            //Confirme as Mudanças
+            ps.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Atualização Concluída");
+        }catch(Exception ex){
+            Logger.getLogger(Registros.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        try{
+            String id = jTextField2.getText(); // Pegar ID
+            
+            //Configure a Conexão do Banco de Dados
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/dbtesting","root","");
+            
+            //Configure o Tipo de Dados e a Atuação
+            PreparedStatement ps = con.prepareStatement("delete from javadbtesting where id=?");
+            
+            //Atribua os Dados | Posição, Dado
+            ps.setString(1, id); // Campo de ID
+            
+            //Confirme as Mudanças
+            ps.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Remoção Concluída");
+        }catch(Exception ex){
+            Logger.getLogger(Registros.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        try{
+            //Recolha os Valores p/ Botar na DB
+            String id = jTextField2.getText(); // Pegar ID
+            
+            //Configure a Conexão do Banco de Dados
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/dbtesting","root","");
+            
+            //Configure o Tipo de Dados e a Atuação
+            PreparedStatement ps = con.prepareStatement("select Valor_1,Valor_2 from javadbtesting where id=?");
+            
+            //Atribua os Dados | Posição, Dado
+            ps.setString(1, id); // Campo de ID
+            
+            //Confirme a Busca
+            ResultSet RS = ps.executeQuery();
+            
+            //Coloque os Valores
+            if(RS.next() == false){
+                System.err.println("Sem Valores Encontrados");
+            }else{
+            jTextField6.setText(RS.getString("Valor_1"));
+            jTextField7.setText(RS.getString("Valor_2"));
+            }
+            
+            JOptionPane.showMessageDialog(null, "Atualização Concluída");
+        }catch(Exception ex){
+            Logger.getLogger(Registros.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -149,15 +267,19 @@ public class Registros extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField4;
     private javax.swing.JTextField jTextField5;
+    private javax.swing.JTextField jTextField6;
+    private javax.swing.JTextField jTextField7;
     // End of variables declaration//GEN-END:variables
 }
